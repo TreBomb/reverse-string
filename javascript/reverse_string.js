912d11b29b662a8c2b47366fc64d3a02424d08ec
Trestin <trestinishak@gmail.com>
2021-10-29 17:09:33 -0500

    finished

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,6 @@
 function reverseString(str) {
-  // type your code here
+  const reversedString = str.split("").reverse().join("");
+  return reversedString;
 }
 
 if (require.main === module) {
@@ -11,9 +12,20 @@ if (require.main === module) {
 
   console.log("Expecting: 'ybabtac'");
   console.log("=>", reverseString("catbaby"));
+
+  console.log("");
+
+  console.log("Expecting: 'ereht olleh'");
+  console.log("=>", reverseString("hello there"));
 }
 
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+
+// put each letter of the string into an array
+// reverse the array
+// join the array into a string
+// return the string
--- ruby/reverse_string.rb
+++ ruby/reverse_string.rb
@@ -1,5 +1,6 @@
 def reverse_string(str)
-  # type your code in here
+  string = str.split("").reverse.join("")
+  string
 end
 
 if __FILE__ == $PROGRAM_NAME
